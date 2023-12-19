0b0046e306b0ddab32455a568cc8c415212eabb1
jeongmijang <153700926+jeongmijang@users.noreply.github.com>
2023-12-19 15:07:24 +0900

    no message

---
+++ jang_p(1)/Assets/Scripts/mon_practice.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mon_practice : MonoBehaviour
+{
+    int id;
+    int hp;
+    int atkPower;
+    int 
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
